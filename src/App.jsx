8b31edf2ee28446780f291b5fd4c27d14ca5d2a9
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-20 14:17:15 +0100

    adding the homepage route path '/' = root

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Products from "./pages/Products";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Products />} />
       </Routes>
     </BrowserRouter>
