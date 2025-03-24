e28bc4a90bace06874a56d50fd33660765c6611f
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-24 10:07:56 +0100

    using the useEffect to fetch data on mount

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,29 @@ import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
   const [cities, setCities] = useState({});
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
 
   return (
     <BrowserRouter>
