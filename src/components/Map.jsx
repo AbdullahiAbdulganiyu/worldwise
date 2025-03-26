4788ae93f7da9ed05236577bf033cf895129b1a7
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-26 14:16:55 +0100

    updating the searchParams when a button is clicked

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -14,6 +14,9 @@ function Map() {
       <h1>
         Position: {lat} {lng}
       </h1>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
+        Change Pos
+      </button>
     </div>
   );
 }
