ee04f5d2560d3682ece257d7eb6b876a45f7214e
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-31 09:37:50 +0100

    disabling some eslint functionality

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -4,6 +4,7 @@ const BASE_URL = "http://localhost:8000";
 
 const CitiesContext = createContext();
 
+// eslint-disable-next-line react/prop-types
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
