d877556bcafd743ac675fdb1c21f3721e45fc750
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-31 10:18:58 +0100

    creating a function that gets the infromation of a city based on the passed id

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -26,6 +26,19 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error loading");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
   return (
     <CitiesContext.Provider
       value={{
