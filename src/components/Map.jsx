6b05e8daa4f8da15ddf198914cdc5720f111d159
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-01 10:00:35 +0100

    changing the url for the tilelayer

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -20,7 +20,7 @@ function Map() {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         <Marker position={[51.505, -0.09]}>
           <Popup>
