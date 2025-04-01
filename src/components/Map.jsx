efd1c16c762c1c6d68dd3445b43d6f0738dd193b
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-01 09:58:42 +0100

    removing the event handler and its dependencies then adding a classname to style the map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,23 @@
 // import { useNavigate, useSearchParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 function Map() {
   //   const [searchParams, setSearchParams] = useSearchParams();
 
-  const navigate = useNavigate();
+  //   const navigate = useNavigate();
   //   const lat = searchParams.get("lat");
   //   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        className={styles.map}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
