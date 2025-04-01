0f703e4119170cbeec377da451a3162f063e6559
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-01 10:31:44 +0100

    fixing some spelling mistakes

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -32,7 +32,7 @@ function Map() {
           >
             <Popup>
               <span>{city.emoji}</span>
-              <span>{city.countryName}</span>
+              <span>{city.cityName}</span>
             </Popup>
           </Marker>
         ))}
