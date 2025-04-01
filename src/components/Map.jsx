0196e2409119f7dc4a34e16989aee9c08481eb37
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-01 10:28:47 +0100

    mapping over the cities and creating a markup for each city

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,6 +2,7 @@
 // import { useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   //   const [searchParams, setSearchParams] = useSearchParams();
@@ -9,6 +10,8 @@ function Map() {
   //   const navigate = useNavigate();
   //   const lat = searchParams.get("lat");
   //   const lng = searchParams.get("lng");
+
+  const { cities } = useCities();
 
   return (
     <div className={styles.mapContainer}>
@@ -22,11 +25,16 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
